32bf69f934ab0ff0f070bd90f349ef95c5355c46
primtimed <brikinibroek@gmail.com>
2023-05-09 19:33:28 +0200

    shooting range start

---
+++ Untitled/Assets/Script/Gun/M4A1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M4A1 : BaseGun
+{
+    public override void Shoot()
+    {
+        if (shootDelay < timer && ammoCount > 0)
+        {
+            base.Shoot();
+            ammoCount -= 1;
+            recoil = new Vector3(recoilMain, 0, 0);
+            cam.transform.eulerAngles -= recoil;
+        }
+    }
+}
