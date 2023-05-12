e619a126308fb52a25c4d3bd2985416dc0e1952a
Dylano Burgers <dylano.burgers@outlook.com>
2023-05-12 14:18:41 +0200

    Input Manager

---
+++ Untitled/Assets/Script/Controller Scripts/ControllerMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMovement : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        
+    }
+}
