5a91d5ce789f5cb5ec849faf18657f6e55e799d5
primtimed <brikinibroek@gmail.com>
2023-06-06 15:46:39 +0200

    brian test ai and portal

---
+++ Untitled/Assets/Script/WaveSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSystem : MonoBehaviour
+{
+    public int roundEnemy;
+    private int spawnedEnemy;
+
+    private GameObject[] inGameEnemy;
+
+    public float spawnSpeed;
+    private float spawnTime;
+
+    public GameObject[] enemy, spawnLoc;
+
+    private void Start()
+    {
+        spawnLoc = GameObject.FindGameObjectsWithTag("Spawns");
+    }
+
+    private void Update()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        spawnTime += Time.deltaTime;
+        inGameEnemy = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (spawnTime >= spawnSpeed && spawnedEnemy <= roundEnemy)
+        {
+            spawnTime = 0;
+            spawnedEnemy += 1;
+
+            Instantiate(enemy[Random.Range(0, enemy.Length)], spawnLoc[Random.Range(0, spawnLoc.Length)].transform.position, spawnLoc[Random.Range(0, spawnLoc.Length)].transform.rotation);
+        }
+
+        if (spawnedEnemy >= roundEnemy && inGameEnemy == null)
+        {
+            print("newWave");
+        }
+    }
+}
