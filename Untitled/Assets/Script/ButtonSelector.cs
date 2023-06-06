ee7bf443aca187bc98ef3c45899444426fd4d8fe
SNIELSEL <niels@haverkotte.nl>
2023-06-06 15:45:50 +0200

    Added mous/controller Ui compatability

---
+++ Untitled/Assets/Script/ButtonSelector.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ButtonSelector : MonoBehaviour
+{
+    public GameObject lastSelectedUI;
+    public InputAction buttonPress;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lastSelectedUI != EventSystem.current.currentSelectedGameObject && EventSystem.current.currentSelectedGameObject != null)
+        {
+            lastSelectedUI = EventSystem.current.currentSelectedGameObject;
+        }
+
+        if(EventSystem.current.currentSelectedGameObject == null)
+        {
+
+            if (lastSelectedUI.GetComponent<Selectable>() != null)
+            {
+                lastSelectedUI.GetComponent<Selectable>().Select();
+            }
+        }
+
+    }
+}
